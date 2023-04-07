66f0f879027f6ce8d9c84983913f317e18d5cf3c
ɳσႦιƚα <127091256+Nobi-2@users.noreply.github.com>
2023-04-07 22:12:51 +0530

    Update fact.js

--- Commands/Fun/fact.js
+++ Commands/Fun/fact.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 
 module.exports = {
   name: "nobi",
-  alias: ["nobita","Nobi","Billa","fact"],
+  alias: ["nobitaa","Nobi","Billa","fact"],
   desc: "Give a random fact.",
   react: "🐈‍⬛",
   category: "Fun",
