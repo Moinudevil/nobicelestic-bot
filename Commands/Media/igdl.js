1dcb4dcd31d57e8b3e809dde57c81964bd8dcc9a
ɳσႦιƚα <127091256+Nobi-2@users.noreply.github.com>
2023-04-09 12:18:44 +0530

    Update igdl.js

--- Commands/Media/igdl.js
+++ Commands/Media/igdl.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 
 module.exports = {
   name: "igdl2",
-  alias: ["instagram2", "instadl2", "instagramdl2", "igvid2", "igdl","ig"],
+  alias: ["instagram2","instadl2", "instagramdl2","igvid2","igdl","ig"],
   desc: "To download an instagram video",
   category: "Media",
   usage: `igdl2 <video link>`,
@@ -11,7 +11,7 @@ module.exports = {
     if (!args[0])
       return Miku.sendMessage(
         m.from,
-        { text: `Please provide a Instagram Video link Baka !` },
+        { text: `Please provide a Instagram Video link Baka😾 !` },
         { quoted: m }
       );
     if (!args[0].includes("instagram.com"))
@@ -27,6 +27,6 @@ module.exports = {
       let res = await axios.get("https://fantox001-scrappy-api.vercel.app/instadl?url=" + queryURL)
       const scrappedURL = res.data.videoUrl
       
-      return Miku.sendMessage(m.from, { video: { url: scrappedURL }, caption: `Downloaded by: *${botName}* \n\n_*Powered by:*_ *ɳσႦιƚα API - by ɳσႦιƚα*\n\n_   \n`},{ quoted: m } );
+      return Miku.sendMessage(m.from, { video: { url: scrappedURL }, caption: `Downloaded by: *${botName}* \n\n_*Powered by:*_ *ɳσႦιƚα API - by ɳσႦιƚα🐈‍⬛*\n\n_   \n`},{ quoted: m } );
   },
 };
